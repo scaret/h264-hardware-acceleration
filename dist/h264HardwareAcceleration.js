0a962ea6f0c6165e89d2455a13c1a8bd88d5021b
scaret <scaret.in@gmail.com>
2022-11-18 17:52:02 +0800

    Hello World

---
+++ dist/h264HardwareAcceleration.js
@@ -0,0 +1,2 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.h264HardwareAcceleration=o():e.h264HardwareAcceleration=o()}(self,(()=>(()=>{"use strict";return console.log("Hello World!"),{}})()));
+//# sourceMappingURL=h264HardwareAcceleration.js.map
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    mode: "production",
+    entry: {
+        h264HardwareAcceleration: "./lib/h264HardwareAcceleration.ts"
+    },
+    output: {
+        devtoolNamespace: 'fakeMediaDevices',
+        path: path.join(__dirname, 'dist'),
+            library: '[name]',
+            libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.js' ],
+    },
+    plugins: [
+    ],
+}
